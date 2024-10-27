90ac026b822a579d0cb6c53dbdc66ddb4cf95857
Minh Long Nguyen <minhlong.nguyen@hdr.qut.edu.au>
2024-10-28 00:41:27 +1000

    update renv

--- code/requiredPackages.R
+++ code/requiredPackages.R
@@ -1,10 +1,11 @@
+# renv::init()
+
 # install.packages("ZVCV")
 # install.packages("rstan")
 # install.packages("partition")
 # install.packages("glmnet")
 # install.packages("rjson")
 # install.packages("xtable")
-
 
 library("ZVCV")
 library("rstan")
